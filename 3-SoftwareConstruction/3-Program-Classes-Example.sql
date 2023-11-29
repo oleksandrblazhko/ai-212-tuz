a33527d920ce1c691dea846c57e9556ce5d1f83c
AnnaTuz <99384383+AnnaTuz@users.noreply.github.com>
2023-11-29 22:35:33 +0100

    Update 3-Program-Classes-Example.sql

--- 3-SoftwareConstruction/3-Program-Classes-Example.sql
+++ 3-SoftwareConstruction/3-Program-Classes-Example.sql
@@ -1,59 +1,47 @@
-
 CREATE OR REPLACE TYPE UserTb_Type AS OBJECT (
-    user_id NUMBER,
     user_name VARCHAR2(30),
     email VARCHAR2(255),
     user_password VARCHAR2(20)
+    user_phone_num VARCHAR2(20)
 );
 /
 
 CREATE OR REPLACE TYPE Cook_Type AS OBJECT (
-    cook_id NUMBER,
-    full_name VARCHAR2(30),
+    cook_name VARCHAR2(30),
     email VARCHAR2(255),
     cook_password VARCHAR2(20)
 );
 /
 
 CREATE OR REPLACE TYPE OrderTb_Type AS OBJECT (
-    order_id NUMBER,
     order_date DATE,
     order_status VARCHAR2(30),
-    user_address VARCHAR2(255),
-    user_num NUMBER,
-    cook_num NUMBER
+    user_address VARCHAR2(255)
 );
 /
 
 CREATE OR REPLACE TYPE RestaurantList_Type AS OBJECT (
-    res_list_id NUMBER,
     res_name VARCHAR2(100),
     category VARCHAR2(50)
 );
 /
 
 CREATE OR REPLACE TYPE Restaurant_Type AS OBJECT (
-    res_id NUMBER,
     res_name VARCHAR2(100),
     description VARCHAR2(500),
-    rating NUMBER,
-    res_list_num NUMBER
+    rating NUMBER
 );
 /
 
 CREATE OR REPLACE TYPE Dish_Type AS OBJECT (
-    dish_id NUMBER,
     dish_name VARCHAR2(100),
     composition VARCHAR2(250),
-    price NUMBER(10, 2),
-    res_num NUMBER
+    price NUMBER(10, 2)
 );
 /
 
 CREATE OR REPLACE TYPE OrderItem_Type AS OBJECT (
     quantity NUMBER,
-    price NUMBER(10, 2),
-    dish_num NUMBER,
-    order_num NUMBER
+    price NUMBER(10, 2)
 );
 /
